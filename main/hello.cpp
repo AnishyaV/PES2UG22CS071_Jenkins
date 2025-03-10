941d340423e281bc09c0c627ccba8124a9d429ac
AnishyaV <114719596+AnishyaV@users.noreply.github.com>
2025-03-10 16:34:51 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
-  cout<< "I have updated hello.cpp";
+  cout << "I have successfully built and run this \n";
+  cout<< "I have updated hello.cpp. ";
   return 0;
 }
